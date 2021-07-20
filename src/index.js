996622c436971d9d86efec159ef87c3779a539bb
Alex Iudin <A.S.Yudinmail@gmail.com>
2021-07-20 13:16:31 +0300

    feat: add function 'min', 'max' and 'avg' for count minimuma , maximuma and average of array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+exports.min = function min(array) {
+    if (array == undefined) return 0;
+    if (array.length == 0) return 0;
+    return array.reduce((prevItem, currentItem) => {
+        return prevItem > currentItem ? currentItem : prevItem;
+    });
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array == undefined) return 0;
+    if (array.length == 0) return 0;
+    return array.reduce((prevItem, currentItem) => {
+        return prevItem > currentItem ? prevItem : currentItem;
+    });
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array == undefined) return 0;
+    if (array.length == 0) return 0;
+    let sum = array.reduce((prevItem, currentItem) => {
+        return prevItem + currentItem;
+    });
+    return sum / array.length;
+};
